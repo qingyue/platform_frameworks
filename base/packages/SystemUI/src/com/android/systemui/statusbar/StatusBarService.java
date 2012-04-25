fab18f31d891f507f487143d37d75e53c327d7d5
qingyue <qingyue@onyx-international.com>
2012-04-25 17:25:37 +0800

    Modify mExpandedDialog refresh

--- base/packages/SystemUI/src/com/android/systemui/statusbar/StatusBarService.java
+++ base/packages/SystemUI/src/com/android/systemui/statusbar/StatusBarService.java
@@ -1260,9 +1260,8 @@ public class StatusBarService extends Service implements CommandQueue.Callbacks 
                 new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                                            ViewGroup.LayoutParams.MATCH_PARENT));
         mExpandedDialog.getWindow().setBackgroundDrawable(null);
-        mExpandedDialog.invalidate(View.UI_GU_MODE);
+        mExpandedDialog.getWindow().getDecorView().invalidate(View.UI_DEFAULT_MODE);
         mExpandedDialog.show();
-        mExpandedDialog.invalidate(View.UI_GU_MODE);
         FrameLayout hack = (FrameLayout)mExpandedView.getParent();
     }
 
