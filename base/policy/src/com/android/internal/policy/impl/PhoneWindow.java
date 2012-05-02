e5968a94c5e62c680acbb49e9466eddde54ea9ec
qingyue <qingyue@onyx-international.com>
2012-05-02 18:38:05 +0800

    OptionsMenu default select the first

--- base/policy/src/com/android/internal/policy/impl/PhoneWindow.java
+++ base/policy/src/com/android/internal/policy/impl/PhoneWindow.java
@@ -460,9 +460,11 @@ public class PhoneWindow extends Window implements MenuBuilder.Callback {
              * Give focus to the view, if it or one of its children does not
              * already have it.
              */
-            if (!st.shownPanelView.hasFocus()) {
-                st.shownPanelView.requestFocus();
-            }
+            //if (!st.shownPanelView.hasFocus()) {
+            //    st.shownPanelView.requestFocus();
+            //}
+
+            st.shownPanelView.requestFocus();
         }
 
         st.isOpen = true;
