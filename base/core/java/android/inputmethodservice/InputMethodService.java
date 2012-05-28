19d42c0dc348f05954b693094b3e3dc41decbbed
qingyue <qingyue@onyx-international.com>
2012-05-28 17:34:08 +0800

    Modify InputMethodService

--- base/core/java/android/inputmethodservice/InputMethodService.java
+++ base/core/java/android/inputmethodservice/InputMethodService.java
@@ -1107,21 +1107,21 @@ public class InputMethodService extends AbstractInputMethodService {
             Log.i(TAG, "IME is show");
             return;
         }
-        if (mOnyxContentFrame.getVisibility() != View.VISIBLE) {
-			mOnyxContentFrame.setVisibility(View.VISIBLE);
-	    }
+        //if (mOnyxContentFrame.getVisibility() != View.VISIBLE) {
+		//	mOnyxContentFrame.setVisibility(View.VISIBLE);
+	    //}
 
-        View onyxView = mInflater.inflate(com.android.internal.R.layout.onyx_input_method_extract_view, null);
-        if (onyxView != null) {
-            mExtractEditText = (ExtractEditText)onyxView.findViewById(com.android.internal.R.id.onyxInputExtractEditText);
-            mExtractEditText.setIME(this);
-            startExtractingText(false);
-            Log.i(TAG, "mExtractEditText : "+mExtractEditText);
-            mOnyxContentFrame.removeAllViews();
-            mOnyxContentFrame.addView(mExtractEditText, new FrameLayout.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.WRAP_CONTENT));
-        }
+        //View onyxView = mInflater.inflate(com.android.internal.R.layout.onyx_input_method_extract_view, null);
+        //if (onyxView != null) {
+        //    mExtractEditText = (ExtractEditText)onyxView.findViewById(com.android.internal.R.id.onyxInputExtractEditText);
+        //    mExtractEditText.setIME(this);
+        //    startExtractingText(false);
+        //    Log.i(TAG, "mExtractEditText : "+mExtractEditText);
+        //    mOnyxContentFrame.removeAllViews();
+        //    mOnyxContentFrame.addView(mExtractEditText, new FrameLayout.LayoutParams(
+        //            ViewGroup.LayoutParams.MATCH_PARENT,
+        //            ViewGroup.LayoutParams.WRAP_CONTENT));
+        //}
 
         //View onyxView = onCreateExtractTextView();
         //Log.i(TAG, "onyxView: "+onyxView);
@@ -1134,6 +1134,20 @@ public class InputMethodService extends AbstractInputMethodService {
         //            com.android.internal.R.id.inputExtractEditText);
         //    mExtractEditText.setIME(this);
         //}
+        
+        if (view != null) {
+            Log.i(TAG, "===setOnyxContentFrameView===");
+            if (mOnyxContentFrame != null) {
+                if (mOnyxContentFrame.getVisibility() != View.VISIBLE) {
+		            mOnyxContentFrame.setVisibility(View.VISIBLE);
+	            }
+
+                Log.i(TAG, "===mOnyxContentFrame.addView===");
+                mOnyxContentFrame.addView(view, new FrameLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            }
+        }
     }
 
     /**
@@ -1577,20 +1591,21 @@ public class InputMethodService extends AbstractInputMethodService {
             else if (newSelEnd > len) newSelEnd = len;
             eet.setSelection(newSelStart, newSelEnd);
             eet.finishInternalChanges();
-        } else if (eet != null && !isFullscreenMode()) {
-            Log.i(TAG, "(eet != null && !isFullscreenMode())");
-            final int off = mExtractedText.startOffset;
-            eet.startInternalChanges();
-            newSelStart -= off;
-            newSelEnd -= off;
-            final int len = eet.getText().length();
-            if (newSelStart < 0) newSelStart = 0;
-            else if (newSelStart > len) newSelStart = len;
-            if (newSelEnd < 0) newSelEnd = 0;
-            else if (newSelEnd > len) newSelEnd = len;
-            eet.setSelection(newSelStart, newSelEnd);
-            eet.finishInternalChanges();
         }
+        //else if (eet != null && !isFullscreenMode()) {
+        //    Log.i(TAG, "(eet != null && !isFullscreenMode())");
+        //    final int off = mExtractedText.startOffset;
+        //    eet.startInternalChanges();
+        //    newSelStart -= off;
+        //    newSelEnd -= off;
+        //    final int len = eet.getText().length();
+        //    if (newSelStart < 0) newSelStart = 0;
+        //    else if (newSelStart > len) newSelStart = len;
+        //    if (newSelEnd < 0) newSelEnd = 0;
+        //    else if (newSelEnd > len) newSelEnd = len;
+        //    eet.setSelection(newSelStart, newSelEnd);
+        //    eet.finishInternalChanges();
+        //}
     }
 
     /**
@@ -1814,7 +1829,8 @@ public class InputMethodService extends AbstractInputMethodService {
                 case KeyEvent.KEYCODE_DPAD_DOWN:
                     return true;
             }
-        } else if (eet != null && !isFullscreenMode()) {
+        } 
+        /*else if (eet != null && !isFullscreenMode()) {
             Log.i(TAG, "(eet != null && !isFullscreenMode())");
             MovementMethod movement = eet.getMovementMethod();
             Layout layout = eet.getLayout();
@@ -1858,7 +1874,7 @@ public class InputMethodService extends AbstractInputMethodService {
                 case KeyEvent.KEYCODE_DPAD_DOWN:
                     return true;
             }
-        }
+        }*/
         
         return false;
     }
@@ -2177,7 +2193,8 @@ public class InputMethodService extends AbstractInputMethodService {
             if (inputChanged) {
                 onExtractingInputChanged(ei);
             }
-        } else if (eet != null && !isFullscreenMode()) {
+        }
+        /*else if (eet != null && !isFullscreenMode()) {
             Log.i(TAG, "else ===startExtractingText===");
             mExtractedToken++;
             ExtractedTextRequest req = new ExtractedTextRequest();
@@ -2217,7 +2234,7 @@ public class InputMethodService extends AbstractInputMethodService {
             if (inputChanged) {
                 onExtractingInputChanged(ei);
             }
-        }
+        }*/
     }
     
     /**
